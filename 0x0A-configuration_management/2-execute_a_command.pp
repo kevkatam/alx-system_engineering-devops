efce27e95c58a5debeb9611f6099b5c96fa94db2
kevkatam <kevkatam@gmail.com>
2023-11-24 06:40:49 -0800

    modified 2-execute_a_command.pp

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # create a manifest that kills a process named killmenow
 exec { 'pkill killmenow':
   path     => '/usr/bin',
-  command  => 'pkill killmenow'
-  provider => shell,
-  returns  => [0, 1]
+  command  => 'pkill killmenow',
+  provider => 'shell',
+  returns  => [0, 1],
 }
